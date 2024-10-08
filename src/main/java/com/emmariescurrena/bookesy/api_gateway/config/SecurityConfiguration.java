78f3aeef20709e472f9936d0379a5be34f01f7ab
emmariescurrena <emmariescurrena>
2024-10-08 09:33:24 -0300

    Disables csrf and adds cors configuration

--- src/main/java/com/emmariescurrena/bookesy/api_gateway/config/SecurityConfiguration.java
+++ src/main/java/com/emmariescurrena/bookesy/api_gateway/config/SecurityConfiguration.java
@@ -1,16 +1,21 @@
 package com.emmariescurrena.bookesy.api_gateway.config;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.server.DefaultServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfiguration {
@@ -26,14 +31,19 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+
         http
+            .csrf((csrf) -> csrf.disable())
+            .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
             .authorizeExchange(authz -> authz
+                .pathMatchers(HttpMethod.POST, "/users").permitAll()
                 .anyExchange().authenticated()
             )
             .oauth2Login(oauth2 -> oauth2
                 .authorizationRequestResolver(authorizationRequestResolver(this.clientRegistrationRepository))
             );
         return http.build();
+
     }
 
     private ServerOAuth2AuthorizationRequestResolver authorizationRequestResolver(
@@ -49,5 +59,21 @@ public class SecurityConfiguration {
     private Consumer<OAuth2AuthorizationRequest.Builder> authorizationRequestCustomizer() {
         return customizer -> customizer
             .additionalParameters(params -> params.put("audience", audience));
-    }   
+    } 
+    
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration
+                .setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 }
