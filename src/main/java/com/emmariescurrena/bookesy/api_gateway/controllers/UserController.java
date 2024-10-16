babff094cf0f5c1d23c7081d07e24094df786377
emmariescurrena <emmariescurrena>
2024-10-16 20:42:34 -0300

    Improves exception handling

--- src/main/java/com/emmariescurrena/bookesy/api_gateway/controllers/UserController.java
+++ src/main/java/com/emmariescurrena/bookesy/api_gateway/controllers/UserController.java
@@ -5,8 +5,12 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.NoFallbackAvailableException;
 import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.ClientAuthorizationRequiredException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,40 +44,37 @@ public class UserController {
     }
 
     @PostMapping
-    public Mono<ResponseEntity<Object>> createUser(@RequestBody CreateUserDto userDto) {
+    public Mono<ResponseEntity<?>> createUser(@RequestBody CreateUserDto userDto) {
         return circuitBreaker.run(
             webClientBuilder.build()
                 .post()
                 .uri("http://user-service/users")
                 .bodyValue(userDto)
-                .exchangeToMono(clientResponse -> {
-                    if (clientResponse.statusCode().is2xxSuccessful()) {
-                        return clientResponse.bodyToMono(User.class)
-                            .map(user -> ResponseEntity.status(clientResponse.statusCode()).body(user));
-                    } else {
-                        return clientResponse.bodyToMono(String.class)
-                            .map(body -> ResponseEntity.status(clientResponse.statusCode()).body(body));
-                    }
-                })
+                .retrieve()
+                .onStatus(HttpStatusCode::isError, clientResponse ->
+                    clientResponse.bodyToMono(String.class)
+                        .flatMap(errorBody -> Mono.error(new RuntimeException(errorBody)))
+                )
+                .toEntity(User.class)
+                .map(userResponse -> ResponseEntity.status(userResponse.getStatusCode()).body(userResponse.getBody())),
+            throwable -> fallbackMethod(throwable)
         );
     }
-    
 
     @GetMapping("/byId/{id}")
     public Mono<ResponseEntity<?>> getUserById(@PathVariable Long id) {
         return circuitBreaker.run(
             webClientBuilder.build()
-            .get()
-            .uri("http://user-service/users/byId/{id}", id)
-            .exchangeToMono(clientResponse -> {
-                if (clientResponse.statusCode().is2xxSuccessful()) {
-                    return clientResponse.bodyToMono(User.class)
-                        .map(user -> ResponseEntity.status(clientResponse.statusCode()).body(user));
-                } else {
-                    return clientResponse.bodyToMono(String.class)
-                        .map(body -> ResponseEntity.status(clientResponse.statusCode()).body(body));
-                }
-            })
+                .get()
+                .uri("http://user-service/users/byId/{id}", id)
+                .retrieve()
+                .onStatus(HttpStatusCode::isError, clientResponse ->
+                    clientResponse.bodyToMono(String.class)
+                        .flatMap(errorBody -> Mono.error(new RuntimeException(errorBody)))
+                )
+                .toEntity(User.class)
+                .map(userResponse -> ResponseEntity.status(userResponse.getStatusCode()).body(userResponse.getBody())),
+            throwable -> fallbackMethod(throwable)
         );
     }
 
@@ -81,18 +82,27 @@ public class UserController {
     public Mono<ResponseEntity<?>> getUserByEmail(@PathVariable String email) {
         return circuitBreaker.run(
             webClientBuilder.build()
-            .get()
-            .uri("http://user-service/users/byEmail/{email}", email)
-            .exchangeToMono(clientResponse -> {
-                if (clientResponse.statusCode().is2xxSuccessful()) {
-                    return clientResponse.bodyToMono(User.class)
-                        .map(user -> ResponseEntity.status(clientResponse.statusCode()).body(user));
-                } else {
-                    return clientResponse.bodyToMono(String.class)
-                        .map(body -> ResponseEntity.status(clientResponse.statusCode()).body(body));
-                }
-            })
+                .get()
+                .uri("http://user-service/users/byEmail/{email}", email)
+                .retrieve()
+                .onStatus(HttpStatusCode::isError, clientResponse ->
+                    clientResponse.bodyToMono(String.class)
+                        .flatMap(errorBody -> Mono.error(new RuntimeException(errorBody)))
+                )
+                .toEntity(User.class)
+                .map(userResponse -> ResponseEntity.status(userResponse.getStatusCode()).body(userResponse.getBody())),
+            throwable -> fallbackMethod(throwable)
         );
+    }
+
+    private Mono<ResponseEntity<?>> fallbackMethod(Throwable throwable) {
+        log.error("Error occurred: ", throwable);
+        if (throwable instanceof ClientAuthorizationRequiredException) {
+            return Mono.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authorization required"));
+        } else if (throwable instanceof NoFallbackAvailableException) {
+            return Mono.just(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Service unavailable"));
+        }
+        return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unknown error occurred"));
     }
 
 }
--- src/main/java/com/emmariescurrena/bookesy/api_gateway/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/emmariescurrena/bookesy/api_gateway/exceptions/GlobalExceptionHandler.java
@@ -1,45 +1,107 @@
 package com.emmariescurrena.bookesy.api_gateway.exceptions;
 
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.cloud.gateway.support.NotFoundException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.security.oauth2.client.ClientAuthorizationRequiredException;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClientRequestException;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ResponseStatusException;
 
-import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import reactor.core.publisher.Mono;
 
-@RestControllerAdvice
-public class GlobalExceptionHandler {
+@Order(Integer.MIN_VALUE)
+public class GlobalExceptionHandler extends AbstractErrorWebExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public Mono<ResponseEntity<ProblemDetail>> handleException(Exception exception) {
-        ProblemDetail errorDetail = null;
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-        exception.printStackTrace();
+    public GlobalExceptionHandler(
+        final ErrorAttributes errorAttributes,
+        final WebProperties.Resources resources,
+        final ApplicationContext applicationContext,
+        final ServerCodecConfigurer configurer
+    ) {
+        super(errorAttributes, resources, applicationContext);
+        setMessageReaders(configurer.getReaders());
+        setMessageWriters(configurer.getWriters());
+    }
 
-        if (exception instanceof WebClientResponseException) {
-            WebClientResponseException webClientEx = (WebClientResponseException) exception;
-            errorDetail = ProblemDetail.forStatusAndDetail(webClientEx.getStatusCode(), webClientEx.getMessage());
-            errorDetail.setProperty("description", "Error in external service call");
-        } else if (exception instanceof WebClientRequestException) {
-            WebClientRequestException webClientEx = (WebClientRequestException) exception;
-            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), webClientEx.getMessage());
-            errorDetail.setProperty("description", "Error in external service call");
-        } else if (exception instanceof AccessDeniedException) {
-            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
-            errorDetail.setProperty("description", "You are not authorized to access this resource");
-        } else if (exception instanceof CallNotPermittedException) {
-            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(503), "Service unavailable due to circuit breaker open");
-            errorDetail.setProperty("description", "The service is temporarily unavailable, please try again later");
+    @Override
+    protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
+        return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse);
+    }
+
+    private Mono<ServerResponse> renderErrorResponse(ServerRequest request) {
+        ErrorAttributeOptions options = ErrorAttributeOptions.of(ErrorAttributeOptions.Include.MESSAGE);
+        Map<String, Object> errorPropertiesMap = getErrorAttributes(request, options);
+        Throwable throwable = getError(request);
+        HttpStatusCode httpStatus = determineHttpStatus(throwable);
+
+        if (throwable instanceof WebClientResponseException) {
+            WebClientResponseException webClientException = (WebClientResponseException) throwable;
+            errorPropertiesMap.put("message", webClientException.getResponseBodyAsString());
+            errorPropertiesMap.put("details", webClientException.getResponseBodyAsString());
+        } else if (throwable instanceof AccessDeniedException) {
+            errorPropertiesMap.put("message", "You do not have permission to access this resource.");
         } else {
-            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), exception.getMessage());
-            errorDetail.setProperty("description", "Unknown internal server error.");
+            errorPropertiesMap.put("message", throwable.getMessage());
         }
 
-        return Mono.just(ResponseEntity.status(errorDetail.getStatus()).body(errorDetail));
+        errorPropertiesMap.put("status", httpStatus.value());
+        errorPropertiesMap.remove("error");
+
+        return ServerResponse.status(httpStatus)
+               .contentType(MediaType.APPLICATION_JSON)
+               .body(BodyInserters.fromValue(errorPropertiesMap));
     }
+
+    private HttpStatusCode determineHttpStatus(Throwable throwable) {
+
+        if (throwable instanceof WebClientResponseException) {
+            return ((WebClientResponseException) throwable).getStatusCode();
+        } else if (throwable instanceof WebClientRequestException) {
+            if (throwable.getCause() instanceof java.net.SocketTimeoutException) {
+                return HttpStatus.GATEWAY_TIMEOUT;
+            } else if (throwable.getCause() instanceof java.net.UnknownHostException) {
+                return HttpStatus.SERVICE_UNAVAILABLE;
+            } else if (throwable.getCause() instanceof javax.net.ssl.SSLException) {
+                return HttpStatus.BAD_GATEWAY;
+            } else {
+                return HttpStatus.SERVICE_UNAVAILABLE;
+            }
+        } else if (throwable instanceof ResponseStatusException) {
+            return ((ResponseStatusException) throwable).getStatusCode();
+        } else if (throwable.getCause() instanceof ClientAuthorizationRequiredException) {
+            return HttpStatus.UNAUTHORIZED;
+        } else if (throwable instanceof NotFoundException) {
+            return HttpStatus.NOT_FOUND;
+        } else if (throwable instanceof AccessDeniedException) {
+            return HttpStatus.FORBIDDEN;
+        } else {
+            // Fallback for unknown exceptions
+            log.error("Unknown error occurred: ", throwable); // Log the exception details
+            return HttpStatus.INTERNAL_SERVER_ERROR;  // Keep the 500 status
+        }
+
+    }
+
 }
