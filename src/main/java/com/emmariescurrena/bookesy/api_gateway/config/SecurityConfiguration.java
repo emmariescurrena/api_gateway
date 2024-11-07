09028017f9745deaee16a81eb70bfd0a30411e11
emmariescurrena <emmariescurrena>
2024-11-07 18:59:42 -0300

    Adds csrf and logout to filterChain and replaces '/users' with '/api/users'

--- src/main/java/com/emmariescurrena/bookesy/api_gateway/config/SecurityConfiguration.java
+++ src/main/java/com/emmariescurrena/bookesy/api_gateway/config/SecurityConfiguration.java
@@ -8,20 +8,33 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.server.DefaultServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.DelegatingServerLogoutHandler;
+import org.springframework.security.web.server.authentication.logout.SecurityContextServerLogoutHandler;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+import org.springframework.security.web.server.authentication.logout.WebSessionServerLogoutHandler;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.server.csrf.CsrfWebFilter;
+import org.springframework.security.web.server.util.matcher.AndServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.NegatedServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfiguration {
     
-    @Value("${okta.oauth2.audience:}")
+    @Value("${okta.oauth2.audience}")
     private String audience;
 
     @Autowired
@@ -31,14 +44,29 @@ public class SecurityConfiguration {
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
 
         http
-            .csrf((csrf) -> csrf.disable())
-            .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .authorizeExchange(authz -> authz
-                .pathMatchers(HttpMethod.POST, "/users/**").permitAll()
+                .pathMatchers(HttpMethod.POST, "/api/users").permitAll()
                 .anyExchange().authenticated()
+            )
+            .csrf(csrf -> csrf
+                .csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
+                .requireCsrfProtectionMatcher(
+                    new AndServerWebExchangeMatcher(
+                        CsrfWebFilter.DEFAULT_CSRF_MATCHER,
+                        new NegatedServerWebExchangeMatcher(
+                            ServerWebExchangeMatchers.pathMatchers(HttpMethod.POST, "/api/users")
+                        )
+                    )
+                )
             )
             .oauth2Login(oauth2 -> oauth2
                 .authorizationRequestResolver(authorizationRequestResolver(this.clientRegistrationRepository))
+            )
+            .logout((logout) -> logout
+                .logoutUrl("/logout")
+                .logoutHandler(logoutHandler)
+                .logoutSuccessHandler(oidcLogoutSuccessHandler())
             );
         return http.build();
 
@@ -57,7 +85,7 @@ public class SecurityConfiguration {
     private Consumer<OAuth2AuthorizationRequest.Builder> authorizationRequestCustomizer() {
         return customizer -> customizer
             .additionalParameters(params -> params.put("audience", audience));
-    } 
+    }
     
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
@@ -65,7 +93,7 @@ public class SecurityConfiguration {
 
         configuration.setAllowedOrigins(List.of("http://localhost:8080"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "XSRF-TOKEN", "X-XSRF-TOKEN", "Cookie"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         
@@ -73,4 +101,21 @@ public class SecurityConfiguration {
 
         return source;
     }
+
+    private DelegatingServerLogoutHandler logoutHandler = new DelegatingServerLogoutHandler(
+            new SecurityContextServerLogoutHandler(),
+            new WebSessionServerLogoutHandler()
+    );
+
+    private ServerLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedServerLogoutSuccessHandler oidcLogoutSuccessHandler =
+                new OidcClientInitiatedServerLogoutSuccessHandler(this.clientRegistrationRepository);
+
+        // Sets the location that the End-User's User Agent will be redirected to
+        // after the logout has been performed at the Provider
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseurl}");
+
+        return oidcLogoutSuccessHandler;
+    }
+
 }
